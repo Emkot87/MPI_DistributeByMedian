7c48059d6710ccb2425d10771d6cc141d06231c4
Emkot87 <49078291+Emkot87@users.noreply.github.com>
2021-12-24 22:05:24 +0200

    Add files via upload

--- benchScript.sh
+++ benchScript.sh
@@ -24,28 +24,28 @@ mpirun -np 16 ./mpiRand.out 32768 386
 mpirun -np 32 ./mpiRand.out 32768 386 
 
 
-echo "--------------------------------------------------------262144 points with 386 diamensions of randomly generated values 0-100"
+echo "--------------------------------------------------------262144 points with 256 diamensions of randomly generated values 0-100"
 
 
-./makeRand.out 262144 386 
+./makeRand.out 262144 256 
 
-mpirun -np 2 ./mpiRand.out 262144 386 
+mpirun -np 2 ./mpiRand.out 262144 256 
 
-mpirun -np 4 ./mpiRand.out 262144 386 
+mpirun -np 4 ./mpiRand.out 262144 256 
 
-mpirun -np 8 ./mpiRand.out 262144 386 
-
-
-echo "--------------------------------------------------------524288 points with 386 diamensions of randomly generated values 0-100"
+mpirun -np 8 ./mpiRand.out 262144 256 
 
 
-./makeRand.out 524288 386 
+echo "--------------------------------------------------------524288 points with 256 diamensions of randomly generated values 0-100"
 
-mpirun -np 2 ./mpiRand.out 524288 386 
 
-mpirun -np 4 ./mpiRand.out 524288 386 
+./makeRand.out 524288 256 
 
-mpirun -np 8 ./mpiRand.out 524288 386 
+mpirun -np 2 ./mpiRand.out 524288 256 
+
+mpirun -np 4 ./mpiRand.out 524288 256 
+
+mpirun -np 8 ./mpiRand.out 524288 256 
 
 echo "--------------------------------------------------------16384 points with 784 diamensions of MNIST"
 
